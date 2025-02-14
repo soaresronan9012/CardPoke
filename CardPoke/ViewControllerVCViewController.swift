d6427625b57657a1683e6bf4472a4a43b82423cb
Ronan Soares <soaresronansilva@gmail.com>
2025-02-13 23:04:43 -0300

    primeiras configuracoes viewcode

---
+++ CardPoke/ViewControllerVCViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerVCViewController.swift
+//  CardPoke
+//
+//  Created by Ronan Soares on 13/02/25.
+//
+
+import UIKit
+
+class ViewControllerVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
