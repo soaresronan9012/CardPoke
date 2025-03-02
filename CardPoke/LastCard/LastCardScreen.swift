e022148a5a18211718afb291de437a58d2c41678
Ronan Soares <soaresronansilva@gmail.com>
2025-03-01 21:55:17 -0300

    criacao da ultima classe de cards

---
+++ CardPoke/LastCard/LastCardScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LastCardScreen.swift
+//  CardPoke
+//
+//  Created by Ronan Soares on 01/03/25.
+//
+
+import UIKit
+
+class LastCardScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
