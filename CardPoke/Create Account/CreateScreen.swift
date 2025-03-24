143658bbfd7b6a446a348614376d904831826fee
Ronan Soares <soaresronansilva@gmail.com>
2025-03-24 20:43:42 -0300

    validacoes textfield e toque na tela para a viewCreate contas

--- CardPoke/Create Account/CreateScreen.swift
+++ CardPoke/Create Account/CreateScreen.swift
@@ -59,9 +59,10 @@ class CreateScreen: UIView {
             let button = UIButton()
             button.translatesAutoresizingMaskIntoConstraints = false
             button.setTitle("Create", for: .normal)
-            button.setTitleColor(.black, for: .normal)
+            button.setTitleColor(.clear, for: .normal)
             //button.backgroundColor = .systemGray5
-            button.backgroundColor = .white.withAlphaComponent(0.5);
+            button.backgroundColor = .clear;
+            button.isEnabled = false
             button.layer.cornerRadius = 10
             //button.layer.borderWidth = 1
             button.layer.borderColor = UIColor.systemGray.cgColor
@@ -73,10 +74,72 @@ class CreateScreen: UIView {
     }
     
     
+    // funcao que baixa o teclado ao tocar na tela
+       private func setupDismissKeyboardGesture() {
+               let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+               tapGesture.cancelsTouchesInView = false // Permite que outros gestos ainda sejam processados
+           self.addGestureRecognizer(tapGesture)
+           }
+       @objc private func dismissKeyboard() {
+           self.endEditing(true) // Fecha o teclado
+           
+           if nameTextFieldCreate.text?.isEmpty ?? true{
+               nameTextFieldCreate.layer.borderColor = UIColor.yellow.cgColor
+               nameTextFieldCreate.layer.borderWidth = 1
+               }
+           if passwordTextFieldCreate.text?.isEmpty ?? true {
+               passwordTextFieldCreate.layer.borderColor = UIColor.yellow.cgColor
+               passwordTextFieldCreate.layer.borderWidth = 1
+               }
+          }
+    
+    
+    public func configDelegateTextField(delegate: UITextFieldDelegate){  // func assinatura do protocol
+        nameTextFieldCreate.delegate = delegate   // elementos a serem validados dentro desse protocol
+        passwordTextFieldCreate.delegate = delegate
+        }
+        
+    
+        // metodo com a validacao dos campos alvos, e tomadas de ações
+         private func validaTextField (){
+             
+             let email: String = nameTextFieldCreate.text ?? " "   //pode receber ou nao valores, possui valor default
+             let password: String = passwordTextFieldCreate.text ?? " "
+                       
+             
+             if !email.isEmpty && !password.isEmpty {   // se estiverem preenchidos faça
+                createAccountButton.isEnabled = true
+                createAccountButton.backgroundColor = .white.withAlphaComponent(0.5);
+                createAccountButton.setTitleColor(.black, for: .normal);
+                }
+             
+             else {
+                 createAccountButton.isEnabled = false
+                 createAccountButton.backgroundColor = .clear;
+                 createAccountButton.setTitleColor(.clear, for: .normal) // placeholder transparente
+            
+                    // gerador de advertencia visual, caso algum campo fique vazio TEXTFIELD
+                    if email.isEmpty {
+                        nameTextFieldCreate.layer.borderColor = UIColor.yellow.cgColor
+                        nameTextFieldCreate.layer.borderWidth = 1
+                        }
+                    if password.isEmpty{
+                        passwordTextFieldCreate.layer.borderColor = UIColor.yellow.cgColor
+                        passwordTextFieldCreate.layer.borderWidth = 1
+                        }
+                }
+        }
+    
+    
+        func callValidaTextField(){ // metodo public invoca método private
+            validaTextField()
+        }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addElements()
         addConstraints()
+        setupDismissKeyboardGesture()
     }
     
     required init?(coder: NSCoder) {
--- CardPoke/Create Account/CreateVC.swift
+++ CardPoke/Create Account/CreateVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class CreateVC: UIViewController {
+class CreateVC: UIViewController, UITextFieldDelegate {
 
     var screen:CreateScreen?
     
@@ -19,9 +19,33 @@ class CreateVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
+        screen?.configDelegateTextField(delegate: self)
         // Do any additional setup after loading the view.
     }
     
-
-   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { //método habilita botao return do teclado
+            textField.resignFirstResponder() // padrao do metodo
+            return true
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) { // fazer as validações sempre aqui
+            //screen?.delegateButtonFunc(delegate: self)
+            screen?.callValidaTextField() // funcao que consta a validacao
+        }
+        
+        func  habilitField () {  // funcao que desabilita advertencia de campo incompleto / #chamada no didbeginediting#
+            if screen?.nameTextFieldCreate.text?.isEmpty == true{
+                //screen?.textName.layer.borderColor = UIColor.red.cgColor
+                screen?.nameTextFieldCreate.layer.borderWidth = 0
+            }
+            if screen?.passwordTextFieldCreate.text?.isEmpty == true {
+                //screen?.textPassword.layer.borderColor = UIColor.red.cgColor
+                screen?.passwordTextFieldCreate.layer.borderWidth = 0        }
+        }
+        
+        
+        func textFieldDidBeginEditing(_ textField: UITextField) { // quando inicia a digitacao
+            screen?.configDelegateTextField(delegate: self)
+            habilitField() // invoca a funcao que limpa advertecia
+        }
 }
